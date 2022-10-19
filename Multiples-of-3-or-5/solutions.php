59b5a22b9a72717f5c1ca3605d37025e529e36d2
Mircea <mirceacojocaru90@gmail.com>
2022-10-19 22:57:11 +0300

    Multiples-of-3-or-5

---
+++ Multiples-of-3-or-5/solutions.php
@@ -0,0 +1,37 @@
+<?php
+
+function solution($number)
+{
+    if ($number < 0) {
+        return 0;
+    }
+
+    $sum = 0;
+
+    $i = 3;
+    while ($i < $number) {
+        $sum += $i;
+        $i += 3;
+    }
+
+    $j = 5;
+    while ($j < $number) {
+        if ($j % 3 !== 0) {
+            $sum += $j;
+        }
+        $j += 5;
+    }
+
+    return $sum;
+}
+
+function bestPracticeSolution($number)
+{
+    $sum = 0;
+    for ($i = 3; $i < $number; $i++) {
+        if ($i % 3 === 0 || $i % 5 === 0) {
+            $sum += $i;
+        }
+    }
+    return $sum;
+}
